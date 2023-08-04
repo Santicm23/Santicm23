502928e73c240322554d134c58e21d9fa1802505
Santicm23 <santicastrom@outlook.com>
2023-08-04 07:12:18 -0500

    creating theme toggle

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,18 +1,22 @@
 import { type JSX } from 'react/jsx-runtime'
+import { ThemeToggle } from '.'
 
 function HeaderBar(): JSX.Element {
   return (
     <header className="fixed top-0 z-50 w-full">
       <nav className="bg-indigo-500/50 backdrop-blur-sm">
-        <ul className="flex justify-center [&>*:hover]:bg-indigo-400">
-          <li className="ml-4 mr-4 rounded-full px-4 py-2">
+        <ul className="flex justify-center">
+          <li className="ml-4 mr-4 rounded-full px-[5%] py-2 hover:bg-indigo-400">
             <a href="#about">About</a>
           </li>
-          <li className="ml-4 mr-4 rounded-full px-4 py-2">
+          <li className="ml-4 mr-4 rounded-full px-[5%] py-2 hover:bg-indigo-400">
             <a href="#projects">Projects</a>
           </li>
-          <li className="ml-4 mr-4 rounded-full px-4 py-2 ">
+          <li className="ml-4 mr-4 rounded-full px-[5%] py-2 hover:bg-indigo-400">
             <a href="#contact">Contact</a>
+          </li>
+          <li className="relative px-10 py-2">
+            <ThemeToggle />
           </li>
         </ul>
       </nav>
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react'
 import { type JSX } from 'react/jsx-runtime'
 
 function ThemeToggle(): JSX.Element {
+  const [theme, setTheme] = useState('light')
+
+  useEffect(() => {
+    if (theme === 'light') {
+      document.documentElement.classList.remove('dark')
+    } else {
+      document.documentElement.classList.add('dark')
+    }
+  }, [theme])
+
+  const toggleTheme = (): void => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
   return (
-    <button>
-      <div></div>
+    <button
+      className="absolute left-[50%] top-[50%] h-[90%] w-[90%] -translate-x-1/2 -translate-y-1/2 rounded-full bg-slate-950 transition-all duration-1000"
+      onClick={toggleTheme}
+    >
+      <div className="relative h-[90%] w-8 rounded-full bg-yellow-300 shadow-[0_0_100px_rgba(249,240,104,1)] transition-all duration-1000 dark:ml-24 dark:bg-gray-100 dark:shadow-[0_0_100px_rgba(255,255,255,1)]">
+        <div className="absolute left-5 top-12 h-3 w-3 rounded-full bg-gray-300/50 opacity-0 transition-all duration-1000 dark:opacity-100"></div>
+        <div className="absolute left-10 top-14 h-3 w-3 rounded-full bg-gray-300/50 opacity-0 transition-all duration-1000 dark:opacity-100"></div>
+        <div className="absolute left-10 top-6 h-5 w-5 rounded-full bg-gray-300/50 opacity-0 transition-all duration-1000 dark:opacity-100"></div>
+      </div>
     </button>
   )
 }
