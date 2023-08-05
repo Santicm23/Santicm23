79848817df04209e63e018a1d14d014c69df02b3
Santicm23 <santicastrom@outlook.com>
2023-08-05 16:37:59 -0500

    publishing to gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App(): JSX.Element {
   return (
     <div className="absolute top-0 h-max min-h-screen w-full">
       <video autoPlay={true} muted loop className="absolute -z-10 h-screen w-full object-cover">
-        <source src="./src/assets/waves.mp4" type="video/mp4" />
+        <source src="./waves.mp4" type="video/mp4" />
       </video>
       <HeaderBar />
       <Name name="Santiago Castro Muñoz" />
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,7 +4,7 @@ function Avatar(): JSX.Element {
   return (
     <img
       className="m-4 h-36 w-36 rounded-full object-cover object-center"
-      src="./src/assets/my-photo.jpg"
+      src="./my-photo.jpg"
       alt="My avatar"
     />
   )
