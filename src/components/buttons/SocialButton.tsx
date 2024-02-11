635c16e076fabc121fd227ce115c5593a507296d
jlcastrocruz <jlcastrocruz@tecficon.com>
2024-02-11 16:23:01 -0500

    minor social buttons adjustment

--- src/components/buttons/SocialButton.tsx
+++ src/components/buttons/SocialButton.tsx
@@ -10,7 +10,7 @@ export default function SocialButton({ Icon, href, name }: Props): JSX.Element {
       <a href={href} target="_blank" rel="noreferrer">
         <Icon className="w-8 duration-200 hover:scale-125 hover:stroke-indigo-400" />
       </a>
-      <span className="absolute -top-14 left-[50%] z-20 origin-left -translate-x-[50%] scale-0 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-bold text-black shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
+      <span className="absolute -bottom-14 left-1/2 z-20 origin-top -translate-x-1/2 scale-0 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-bold text-black shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
         {name}
         <span></span>
       </span>
