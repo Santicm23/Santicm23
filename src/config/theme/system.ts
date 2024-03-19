33128fc90de1537eb19ba0fe62f7a97dd0cfbe9d
Santicm23 <santicastrom@outlook.com>
2024-03-19 17:40:37 -0500

    migrating 90% of portfolio

--- src/config/theme/system.ts
+++
@@ -1,9 +0,0 @@
-export function setSystemTheme(): void {
-  if (localStorage.getItem("theme") === "dark") {
-    document.documentElement.classList.add("dark");
-  } else if (localStorage.getItem("theme") === "light") {
-    document.documentElement.classList.remove("dark");
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.classList.add("dark");
-  }
-}
