c8741b648e2253ce95c33cb867ee2778bae48cde
Santicm23 <Santicastrom@outlook.com>
2024-03-19 21:59:31 -0500

    migrating to astro + react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
+  output: 'server'
 });
