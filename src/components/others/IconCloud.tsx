a7b5e84b60fd1a320b079a1a85df388aa094a529
Santicm23 <santicastrom@outlook.com>
2024-07-05 16:06:28 -0500

    fixing responsive issues

--- src/components/others/IconCloud.tsx
+++ src/components/others/IconCloud.tsx
@@ -10,13 +10,48 @@ import {
 import useTheme from "@/hooks/useTheme"
 import { isDarkColor, isLightColor } from "@/utils/colors"
 
+const slugs = [
+	"typescript",
+	"java",
+	"python",
+	"cplusplus",
+	"rust",
+	"dart",
+	"kotlin",
+	"react",
+	"angular",
+	"astro",
+	"android",
+	"flutter",
+	"nextdotjs",
+	"tailwindcss",
+	"express",
+	"nestjs",
+	"fastapi",
+	"springboot",
+	"rabbitmq",
+	"nginx",
+	"prisma",
+	"poetry",
+	"selenium",
+	"git",
+	"docker",
+	"github",
+	"tensorflow",
+	"scikitlearn",
+]
+
 const cloudProps: Omit<ICloud, "children"> = {
 	containerProps: {
 		style: {
+			position: "absolute",
+			top: 0,
+			left: 0,
+			right: 0,
+			bottom: 0,
 			display: "flex",
 			justifyContent: "center",
 			alignItems: "center",
-			height: "12rem",
 		},
 	},
 	options: {
@@ -32,9 +67,9 @@ const cloudProps: Omit<ICloud, "children"> = {
 		outlineColour: "#0000",
 		maxSpeed: 0.03,
 		minSpeed: 0.01,
-		radiusX: 0.8,
-		radiusY: 0.8,
-		radiusZ: 0.8,
+		radiusX: 0.75,
+		radiusY: 0.75,
+		radiusZ: 0.75,
 	},
 }
 
@@ -74,15 +109,15 @@ export type DynamicCloudProps = {
 
 type IconData = Awaited<ReturnType<typeof fetchSimpleIcons>>
 
-export default function IconCloud({ iconSlugs }: DynamicCloudProps) {
+export default function IconCloud() {
 	const [data, setData] = useState<IconData | null>(null)
 
 	const { isDarkMode } = useTheme()
 	const theme = isDarkMode ? "dark" : "light"
 
 	useEffect(() => {
-		fetchSimpleIcons({ slugs: iconSlugs }).then(setData)
-	}, [iconSlugs])
+		fetchSimpleIcons({ slugs }).then(setData)
+	}, [])
 
 	const renderedIcons = useMemo(() => {
 		if (!data) return null
