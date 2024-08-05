4af35d8484df7d7a9c7d5a784b21fc4a3dd11d5f
Santicm23 <santicastrom@outlook.com>
2024-08-04 23:48:33 -0500

    adding vercel dependencies to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel()
 });
