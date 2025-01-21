e4885a53b8428b2345cbef3dbecdc4983f3dd552
Santicm23 <santicastrom@outlook.com>
2025-01-21 16:39:49 -0500

    adding loader and adjusting the 3D piano size

--- src/components/others/Piano3D.tsx
+++ src/components/others/Piano3D.tsx
@@ -1,9 +1,14 @@
-import Spline from "@splinetool/react-spline"
+import { lazy, Suspense } from 'react';
+import Loader from '../loaders/Loader';
+
+const Spline = lazy(() => import('@splinetool/react-spline'));
 
 export default function Piano3D() {
 	return (
-		<div className="h-[470px] w-full">
-			<Spline scene="https://prod.spline.design/nNoIkSdzguX8mdUC/scene.splinecode" />
-		</div>
+		<Suspense fallback={<Loader />}>
+			<div className="h-[470px] w-full">
+				<Spline scene="https://prod.spline.design/nNoIkSdzguX8mdUC/scene.splinecode" />
+			</div>
+		</Suspense>
 	)
 }
