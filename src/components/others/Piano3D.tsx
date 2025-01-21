de4d60b7a4e9b8fb431ab82f1ec8373e6571e0d3
Santicm23 <santicastrom@outlook.com>
2025-01-21 01:04:25 -0500

    adding 3d piano to portfolio

---
+++ src/components/others/Piano3D.tsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Piano3D() {
+  return (
+    <Spline scene="https://prod.spline.design/nNoIkSdzguX8mdUC/scene.splinecode" />
+  );
+}
