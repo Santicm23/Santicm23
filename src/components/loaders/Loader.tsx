1e2dcc0309b89502d32fd9f53bd320fbfeb7c1f0
Santicm23 <santicastrom@outlook.com>
2025-01-22 11:26:38 -0500

    finishing contact me card

--- src/components/loaders/Loader.tsx
+++ src/components/loaders/Loader.tsx
@@ -2,7 +2,7 @@ import "./Loader.css"
 
 export default function Loader() {
   return (
-    <div className="container">
+    <div className="loader">
       <div className="half before:bg-indigo-400 after:bg-indigo-400 dark:before:bg-violet-400 dark:after:bg-violet-400"></div>
       <div className="half before:bg-indigo-400 after:bg-indigo-400 dark:before:bg-violet-400 dark:after:bg-violet-400"></div>
     </div>
--- src/components/others/Piano3D.tsx
+++ src/components/others/Piano3D.tsx
@@ -1,13 +1,13 @@
-import { lazy, Suspense } from 'react';
-import Loader from '../loaders/Loader';
+import { lazy, Suspense } from "react"
+import Loader from "../loaders/Loader"
 
-const Spline = lazy(() => import('@splinetool/react-spline'));
+const Spline = lazy(() => import("@splinetool/react-spline"))
 
 export default function Piano3D() {
 	return (
 		<Suspense fallback={<Loader />}>
-			<div className="h-[470px] w-full">
-				<Spline scene="https://prod.spline.design/nNoIkSdzguX8mdUC/scene.splinecode" />
+			<div className="h-[470px] w-full max-w-[300px]">
+				<Spline scene="https://draft.spline.design/wRe6yRj-PkMn1cGo/scene.splinecode" />
 			</div>
 		</Suspense>
 	)
