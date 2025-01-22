e24789b49461fc1279666b06739e819e3d3d3a8b
Santicm23 <santicastrom@outlook.com>
2025-01-22 18:13:14 -0500

    fixing scroll problem on piano and fixing weird form behavior

--- src/components/others/Piano3D.tsx
+++ src/components/others/Piano3D.tsx
@@ -7,7 +7,7 @@ export default function Piano3D() {
 	return (
 		<Suspense fallback={<Loader />}>
 			<div className="h-[470px] w-full max-w-[300px]">
-				<Spline scene="https://draft.spline.design/wRe6yRj-PkMn1cGo/scene.splinecode" />
+				<Spline scene="https://draft.spline.design/1nt5EcSeZB3b4RHY/scene.splinecode" />
 			</div>
 		</Suspense>
 	)
