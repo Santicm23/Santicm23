757c23758dcb6306b48aff22c87ecb826a0ecca3
Santicm23 <santicastrom@outlook.com>
2025-01-22 18:22:37 -0500

    fixing scroll

--- src/components/others/Piano3D.tsx
+++ src/components/others/Piano3D.tsx
@@ -7,7 +7,7 @@ export default function Piano3D() {
 	return (
 		<Suspense fallback={<Loader />}>
 			<div className="h-[470px] w-full max-w-[300px]">
-				<Spline scene="https://draft.spline.design/1nt5EcSeZB3b4RHY/scene.splinecode" />
+				<Spline scene="https://draft.spline.design/kPsEPJGK8-QVUBQc/scene.splinecode" />
 			</div>
 		</Suspense>
 	)
